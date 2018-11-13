84749400b004c6f0cccb02b0c7c7d3f1650db13c
janehuang1647 <jane1647@hotmail.com>
2018-11-13 12:54:22 -0800

    add testing

--- tests/testthat/test_df_read_write.R
+++ tests/testthat/test_df_read_write.R
@@ -16,4 +16,22 @@ test_that("simple write and read",{
 
 
 
+test_that("test whether it returns a factor",{
+  df <- data.frame(
+    testing = factor(c("a","b","c","a","c","e"), levels = c("a", "b","c","e"),
+                     labels = c("A", "B","C","E")))
 
+
+  df_write(df,"test1.txt")
+  read_data <- df_read("test1.txt")
+
+  expect_equal(is.factor(df$testing),is.factor(read_data$testing))
+
+})
+
+
+test_that("test if an file doesn't exist, it will raise error",{
+
+  expect_error(read_data("no_such_file.txt"))
+
+})
